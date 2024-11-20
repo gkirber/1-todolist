e9671ba0f728eab044e000f99136e3b601b9047a
gkirber <george.kirber@gmail.com>
2024-11-20 22:47:49 +0100

    s1_w1

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {Todolist} from './Todolist'
+import {v4 as uuidv4} from 'uuid';
 
-function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+export type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
 }
 
-export default App;
+function App() {
+    const tasks1 = [
+        {id: uuidv4(), title: 'HTML&CSS', isDone: true},
+        {id: uuidv4(), title: 'JS', isDone: true},
+        {id: uuidv4(), title: 'ReactJS', isDone: false},
+        {id: uuidv4(), title: 'Redux', isDone: false},
+        {id: uuidv4(), title: 'Typescript', isDone: false},
+        {id: uuidv4(), title: 'RTK query', isDone: false},
+    ]
+
+    const tasks2: TaskType[] = [];
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1} date={'30.01.2024'}/>
+            <Todolist title="Songs" tasks={tasks2}/>
+        </div>
+    )
+}
+
+export default App
